9cfa4c438efb9940ffef9d80c74ca39ae0042c5e
SpookyDervish <nchand05@outlook.com>
2025-05-12 13:34:23 +1000

    added more functionality

--- server/db.py
+++ server/db.py
@@ -39,9 +39,16 @@ class Database:
         self.commit()
 
         server_id = self.create_server("Testing Server")
+        server2_id = self.create_server("Testing Server 2")
         user_id = self.create_user("SpookyDervish")
 
         self.add_user_to_server(user_id, server_id)
+
+        print(self.users_in_server("Testing Server"))
+        print(self.is_user_in_server(user_id, server_id))
+        print(self.is_user_in_server(user_id, server2_id))
+
+        self.close()
 
     def commit(self):
         self.conn.commit()
@@ -60,10 +67,37 @@ class Database:
         return user_id
     
     def add_user_to_server(self, user_id: int, server_id: int):
+        if self.is_user_in_server(user_id, server_id):
+            return
+
         memberships = [
             (user_id, server_id)
         ]
         self.cur.executemany("INSERT INTO memberships (user_id, server_id) VALUES (?, ?)", memberships)
+
+    def user_exists(self, user_id: int):
+        self.cur.execute("SELECT 1 FROM users WHERE user_id = ? LIMIT 1", (user_id,))
+        return self.cur.fetchone() is not None
+
+    def user_exists_by_name(self, username: str):
+        self.cur.execute("SELECT 1 FROM users WHERE username = ? LIMIT 1", (username,))
+        return self.cur.fetchone() is not None
+    
+    def server_exists(self, server_id: int):
+        self.cur.execute("SELECT 1 FROM servers WHERE server_id = ? LIMIT 1", (server_id,))
+        return self.cur.fetchone() is not None
+    
+    def server_exists_by_name(self, name: str):
+        self.cur.execute("SELECT 1 FROM servers WHERE name = ? LIMIT 1", (name,))
+        return self.cur.fetchone() is not None
+
+    def is_user_in_server(self, user_id: int, server_id: int):
+        self.cur.execute("""
+            SELECT 1 FROM memberships
+            WHERE user_id = ? AND server_id = ?
+            LIMIT 1
+        """, (user_id, server_id))
+        return self.cur.fetchone() is not None
 
     def users_in_server(self, server_name: str):
         self.cur.execute("""
