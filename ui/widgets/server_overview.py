ebbb828017e4bb7122c72b35286e963e9c4ec774
SpookyDervish <78246495+SpookyDervish@users.noreply.github.com>
2025-05-13 17:03:52 +1000

    fixed invalid markup crash in server descriptions

--- ui/widgets/server_overview.py
+++ ui/widgets/server_overview.py
@@ -1,6 +1,8 @@
 from textual.containers import Vertical
 from textual.widgets import Label
 
+from textual.markup import MarkupError
+
 
 class ServerOverview(Vertical):
     DEFAULT_CSS = """
@@ -21,4 +23,12 @@ class ServerOverview(Vertical):
         self.info = server_info
 
     def compose(self):
-        yield Label(f"[b u]Welcome to {self.info['title']}[/b u]\n[b]Online: [green]{self.info['online']}[/green][/b]\n\n{self.info['description']}")
+        final_text = f"[b u]Welcome to {self.info['title']}[/b u]\n[b]Online: [green]{self.info['online']}[/green][/b]\n\n{self.info['description']}"
+        label = Label(final_text)
+
+        try:
+            label.visual
+        except MarkupError:
+            label = Label(final_text, markup=False)
+
+        yield label
