a8df876b8d1634fa6e02acb4e33588ea3f8d3405
SpookyDervish <78246495+SpookyDervish@users.noreply.github.com>
2025-05-16 17:23:30 +1000

    added a default description and title for servers

--- ui/widgets/create_server_menu.py
+++ ui/widgets/create_server_menu.py
@@ -5,6 +5,36 @@ from textual.widgets import Label, Input, TextArea, Button
 from server.server import Server
 
 
+DEFAULT_SERVER_DESCRIPTION = """
+Hello! Welcome to your server! Here are some things you can do to make your description more interesting! :)
+
+Styles:
+[bold]This text will be bold.[/bold]
+
+[italic]This text will be italic.[/italic]
+
+[dim]This text will be dim.[/dim]
+
+[underline]This text will have an underline (if you mix this with bold, it looks like a title!)[/underline]
+
+[strike]This text will have a strike through it.[/strike]
+
+[reverse]This text will be reversed. (the background and the foreground colours are swapped.)[/reverse]
+
+[blink]This text will blink! (Good for getting the user's attention ;))[/blink]
+
+[red]This text will be red, but you can replace "red" with any colour you like![/red]
+
+[red on black]This text has a black background, and a red foreground![/red on black]
+
+
+Some combined styles:
+[bold underline]This text is bold, AND has an underline![/bold underline]
+
+[bold black on yellow blink]This text is bold, will blink, its background colour is yellow, and its foreground colour is black![/bold black on yellow]
+"""
+
+
 class CreateServerScreen(ModalScreen):
     DEFAULT_CSS = """
     CreateServerScreen {
@@ -57,10 +87,10 @@ class CreateServerScreen(ModalScreen):
             window.border_title = "=== Start Server ==="
             with HorizontalGroup():
                 yield Label("Server title:")
-                yield Input(placeholder="Enter a server title!")
+                yield Input("My Cool Server", placeholder="Enter a server title!")
 
             yield Label("Server Description:")
-            yield TextArea("the [bold yellow reverse]COOLEST[/bold yellow reverse] server", language="xml", id="desc-input")
+            yield TextArea(DEFAULT_SERVER_DESCRIPTION, id="desc-input")
 
             with Horizontal():
                 yield Button("Cancel", id="cancel-button")
