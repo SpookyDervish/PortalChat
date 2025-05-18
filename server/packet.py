772a58b3cf23057183fc9afae4d11119130fe6a1
SpookyDervish <78246495+SpookyDervish@users.noreply.github.com>
2025-05-19 07:30:22 +1000

    handle timestamps in packets

--- server/packet.py
+++ server/packet.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, asdict
 from typing import Any
 from enum import Enum
+from datetime import datetime
 
 import msgpack
 
@@ -27,12 +28,17 @@ class Packet:
 
 
 def to_bytes(packet: Packet):
-        # convert the packet to a dict, and then convert it to be bytes
-        # so it can be sent over the socket
-        packet_type = packet.packet_type.value
-        packet.packet_type = packet_type
+    # convert the packet to a dict, and then convert it to be bytes
+    # so it can be sent over the socket
+    packet_type = packet.packet_type.value
+    packet.packet_type = packet_type
 
-        return msgpack.packb(asdict(packet))
+    # ensure timestamps are converted to strings to avoid issues
+    if packet.data.get("timestamp"):
+        packet.data["timestamp"] = datetime.strptime(packet.data["timestamp"], "%Y-%m-%d %H:%M:%S")
+
+    # return the bytes of the packet
+    return msgpack.packb(asdict(packet))
 
 def to_packet(data):
     unpacked = msgpack.unpackb(data, raw=False) # convert the bytes to a dict
