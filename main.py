6cb25273b645c431357e3518776c655b7a8e7eea
SpookyDervish <nchand05@outlook.com>
2025-05-19 13:17:41 +1000

    auto install needed packages

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from ui.app import Portal
+import subprocess, sys
 
 
 if __name__ == "__main__":
+    try:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+    except subprocess.CalledProcessError as e:
+        print(f"Installation failed: {e}")
+        sys.exit(1)
     app = Portal()
     app.run()
